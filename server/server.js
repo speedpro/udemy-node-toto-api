5f2faceb0d532f85611ebb750f231c89c8aa84ec
Caleb McDonnell <caleb@mcdonnell.geek.nz>
2017-05-22 23:36:01 +1200

    get ready for heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
 const {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require("./models/user");
 var {Todo} = require("./models/todo");
+var {User} = require("./models/user");
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log(`Server started on port 3000`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports.app = app;
