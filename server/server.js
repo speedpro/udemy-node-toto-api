046f04af50813774f388424d8599651f572fcac4
Caleb McDonnell <caleb@mcdonnell.geek.nz>
2017-05-22 22:33:06 +1200

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -25,11 +25,11 @@ app.get("/todos", (req,res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
-})
+  });
+});
 
 app.listen(3000, () => {
   console.log(`Server started on port 3000`);
-})
+});
 
 module.exports.app = app;
