6e5a79818920c7ed8e622fbca97c97bc1e42d96d
Caleb McDonnell <caleb@mcdonnell.geek.nz>
2017-05-25 20:23:05 +1200

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,14 @@ UserSchema.methods.generateAuthToken = function () {
   })
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({$pull: {
+    tokens: {token}
+  }});
+}
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
