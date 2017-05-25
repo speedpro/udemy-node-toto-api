383db18c6f4c79c32e33f4c6db251906d3734a8d
Caleb McDonnell <caleb@mcdonnell.geek.nz>
2017-05-25 18:41:14 +1200

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,8 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
+const bcrypt = require('bcryptjs');
+
 var UserSchema = new mongoose.Schema({
   email: {
     required: true,
@@ -49,6 +51,23 @@ UserSchema.methods.generateAuthToken = function () {
     return token;
   })
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err,salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    });
+
+  } else {
+    next();
+  }
+});
+
 
 UserSchema.statics.findByToken = function (token) {
   var User = this;
